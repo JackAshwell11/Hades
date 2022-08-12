b2164bfe98ba3484cbd75a53349368752f4c6d0b
Aspect1103 <jack.ashwell1@gmail.com>
2022-08-12 20:21:32 +0100

    Updated __init__.py's docstring.
    Updated poetry homepage and repository links.

--- hades/__init__.py
+++ hades/__init__.py
@@ -1,15 +1,1 @@
-"""A procedurally generated dungeon crawler bullet-hell made in Python.
-
-This game uses `Arcade <https://api.arcade.academy/en/latest/>`_ to visualise the game
-and make it playable, `Numpy <https://numpy.org>`_ for the procedural generation,
-`Pymunk <https://www.pymunk.org/en/latest/>`_ for the physics simulation and `Shapely
-<https://shapely.readthedocs.io/en/stable/>`_ to speed up various parts of the Arcade
-library.
-
-:copyright: (c) 2021-present Aspect1103
-:license: MIT, see LICENSE for more details.
-"""
-
-__author__ = "Aspect1103"
-__license__ = "MIT"
-__version__ = "0.0.1"
+"""Stores all the functionality which creates the game and makes it playable."""
