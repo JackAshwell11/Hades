1aaac699aee54fb901edf4f9ef147b72a5a7941d
Aspect1103 <jack.ashwell1@gmail.com>
2023-03-10 11:24:48 +0000

    Added CLI tool to build.py to allow multiple different build options.
    Rearranged Makefile.
    
    Added build.py to poetry installation.
    
    Added cargo updater to dependabot.

--- build.py
+++ build.py
@@ -2,49 +2,92 @@
 from __future__ import annotations
 
 # Builtin
+import argparse
 import platform
 import subprocess
 import zipfile
 from pathlib import Path
+from typing import Any
 
-# Initialise some constants
-GAME_NAME = "python_src/hades"
-SOURCE_DIR = Path().absolute() / GAME_NAME / "window.py"
-RESOURCES_DIR = f"{GAME_NAME}/resources"
-OUTPUT_DIR = Path().absolute() / "build"
-DIST_DIR = OUTPUT_DIR.joinpath("window.dist")
-ZIP_OUTPUT_NAME = f"{GAME_NAME}.zip"
 
-# Display some info about the system and the environment
-print(
-    f"System information: {platform.system()} {platform.version()}. Python version:"
-    f" {platform.python_implementation()} {platform.python_version()}"
-)
-print(f"Current directory: {Path().absolute()}")
+def executable() -> None:
+    """Compiles the game into an executable format for portable use."""
+    # Initialise some constants
+    game_dir = "python_src/hades"
+    source_dir = Path().absolute() / game_dir / "window.py"
+    resources_dir = f"{game_dir}/resources"
+    output_dir = Path().absolute() / "build"
+    dist_dir = output_dir.joinpath("window.dist")
+    zip_output_name = f"{game_dir}.zip"
 
-# Create the command to build the game
-COMMANDS = [
-    ".\\.venv\\Scripts\\python.exe -m",
-    f'nuitka "{SOURCE_DIR}"',
-    "--standalone",
-    "--assume-yes-for-downloads",
-    f"--include-data-dir={RESOURCES_DIR}={RESOURCES_DIR}",
-    f'--output-dir="{OUTPUT_DIR}"',
-    "--plugin-enable=numpy",
-]
-COMMAND_STRING = " ".join(COMMANDS)
-print(f"Executing command string: {COMMAND_STRING}")
+    # Display some info about the system and the environment
+    print(
+        f"System information: {platform.system()} {platform.version()}. Python version:"
+        f" {platform.python_implementation()} {platform.python_version()}"
+    )
+    print(f"Current directory: {Path().absolute()}")
 
-# Execute the build command
-subprocess.run(COMMAND_STRING, check=True)
+    # Create the command to build the game
+    commands = [
+        ".\\.venv\\Scripts\\python.exe -m",
+        f'nuitka "{source_dir}"',
+        "--standalone",
+        "--assume-yes-for-downloads",
+        f"--include-data-dir={resources_dir}={resources_dir}",
+        f'--output-dir="{output_dir}"',
+        "--plugin-enable=numpy",
+    ]
+    command_string = " ".join(commands)
+    print(f"Executing command string: {command_string}")
 
-# Zip the game and verify that the file exists
-with zipfile.ZipFile(ZIP_OUTPUT_NAME, "w", zipfile.ZIP_DEFLATED) as zip_file:
-    for build_file in DIST_DIR.rglob("*"):
-        zip_file.write(build_file, str(build_file).replace(str(DIST_DIR), ""))
-print(f"Finishing zipping {ZIP_OUTPUT_NAME}. Now verifying archive")
-try:
-    assert Path(ZIP_OUTPUT_NAME).exists()
-    print(f"Output of {ZIP_OUTPUT_NAME} was successful")
-except AssertionError:
-    print(f"Output of {ZIP_OUTPUT_NAME} was unsuccessful")
+    # Execute the build command
+    subprocess.run(command_string, check=True)
+
+    # Zip the game and verify that the file exists
+    with zipfile.ZipFile(zip_output_name, "w", zipfile.ZIP_DEFLATED) as zip_file:
+        for build_file in dist_dir.rglob("*"):
+            zip_file.write(build_file, str(build_file).replace(str(dist_dir), ""))
+    print(f"Finishing zipping {zip_output_name}. Now verifying archive")
+    try:
+        assert Path(zip_output_name).exists()
+        print(f"Output of {zip_output_name} was successful")
+    except AssertionError:
+        print(f"Output of {zip_output_name} was unsuccessful")
+
+
+def rust() -> None:
+    """Compiles the Rust extensions and installs them into the virtual environment."""
+    # Compile the Rust extensions and install them
+    subprocess.run("maturin develop", check=True)
+
+
+def build(_: dict[str, Any]) -> None:
+    """Allow Poetry to automatically build the Rust extensions upon installation."""
+    rust()
+
+
+if __name__ == "__main__":
+    # Build the argument parser and start parsing arguments
+    parser = argparse.ArgumentParser(
+        description="Simplifies building/compiling related to Hades"
+    )
+    build_group = parser.add_mutually_exclusive_group()
+    build_group.add_argument(
+        "-e",
+        "--executable",
+        action="store_true",
+        help="compiles the game into an executable format",
+    )
+    build_group.add_argument(
+        "-r",
+        "--rust",
+        action="store_true",
+        help="compiles the Rust extensions and installs them",
+    )
+    args = parser.parse_args()
+
+    # Determine which argument was selected
+    if args.executable:
+        executable()
+    elif args.rust:
+        rust()
