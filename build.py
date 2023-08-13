a2d4edd0e6b44803d9f53daa99c2b5573123705d
Aspect1103 <jack.ashwell1@gmail.com>
2023-08-13 14:38:49 +0100

    Commented out mypy pre-commit hook.
    Refactored parts of build.py.

--- build.py
+++ build.py
@@ -7,10 +7,15 @@ import platform
 import subprocess
 import zipfile
 from pathlib import Path
+from typing import Final
 
 # Pip
 from setuptools import Extension, setup
 from setuptools.command.build_ext import build_ext
+
+# Initialise some constants for the executable command
+RESOURCES_DIR: Final = "src/hades/resources"
+ZIP_OUTPUT_NAME: Final = "hades.zip"
 
 
 class BuildNamespace(argparse.Namespace):
@@ -29,10 +34,8 @@ class CMakeBuild(build_ext):
         Args:
             ext: The extension to build.
         """
-        # Determine the current directory and the profile to build the CMake extension
-        # with
+        # Determine the current directory to build the CMake extension with
         current_dir = Path(__file__).parent
-        profile = "Release"
 
         # Determine where the extension should be transferred to after it has been
         # compiled
@@ -50,75 +53,62 @@ class CMakeBuild(build_ext):
                     "cmake",
                     str(current_dir.joinpath(ext.sources[0])),
                     "-DDO_PYTHON=true",
-                    f"-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{profile.upper()}={build_dir}",
+                    f"-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_RELEASE={build_dir}",
                 ],
             ),
             cwd=build_temp,
             check=True,
         )
-        subprocess.run(
-            f"cmake --build . --config {profile}",
-            cwd=build_temp,
-            check=True,
-        )
+        subprocess.run("cmake --build . --config Release", cwd=build_temp, check=True)
 
 
 def executable() -> None:
     """Compiles the game into an executable format for portable use."""
-    # Initialise some constants
-    game_dir = "src/hades"
-    source_dir = Path().absolute() / game_dir / "window.py"
-    resources_dir = f"{game_dir}/resources"
-    output_dir = Path().absolute() / "build"
+    # Initialise some paths
+    current_dir = Path(__file__).parent
+    source_dir = current_dir.joinpath("src").joinpath("hades").joinpath("window.py")
+    output_dir = current_dir.joinpath("build")
     dist_dir = output_dir.joinpath("window.dist")
-    zip_output_name = f"{game_dir}.zip"
 
     # Display some info about the system and the environment
     print(
-        f"System information: {platform.system()} {platform.version()}. Python"
-        f" version: {platform.python_implementation()} {platform.python_version()}",
+        f"System information: {platform.system()} {platform.version()}. Python version:"
+        f" {platform.python_implementation()} {platform.python_version()}",
     )
-    print(f"Current directory: {Path().absolute()}")
-
-    # Create the command to build the game
-    commands = [
-        ".\\.venv\\Scripts\\python.exe -m",
-        f'nuitka "{source_dir}"',
-        "--standalone",
-        "--assume-yes-for-downloads",
-        f"--include-data-dir={resources_dir}={resources_dir}",
-        f'--output-dir="{output_dir}"',
-        "--plugin-enable=numpy",
-    ]
-    command_string = " ".join(commands)
-    print(f"Executing command string: {command_string}")
 
     # Execute the build command
-    subprocess.run(command_string, check=True)
+    subprocess.run(
+        " ".join(
+            [
+                "./.venv/Scripts/python.exe -m",
+                f'nuitka "{source_dir}"',
+                "--standalone",
+                "--assume-yes-for-downloads",
+                f"--include-data-dir={RESOURCES_DIR}={RESOURCES_DIR}",
+                f'--output-dir="{output_dir}"',
+                "--remove-output",
+            ],
+        ),
+        check=True,
+    )
 
-    # Zip the game and verify that the file exists
-    with zipfile.ZipFile(zip_output_name, "w", zipfile.ZIP_DEFLATED) as zip_file:
+    # Zip the game and display the result
+    with zipfile.ZipFile(ZIP_OUTPUT_NAME, "w", zipfile.ZIP_DEFLATED) as zip_file:
         for build_file in dist_dir.rglob("*"):
             zip_file.write(build_file, str(build_file).replace(str(dist_dir), ""))
-    print(f"Finishing zipping {zip_output_name}. Now verifying archive")
-    try:
-        print(f"Output of {zip_output_name} was successful")
-    except AssertionError:
-        print(f"Output of {zip_output_name} was unsuccessful")
 
 
 def cpp() -> None:
     """Compiles the C++ extensions and installs them into the virtual environment."""
-    dist = setup(
-        name="hades_extensions",
-        ext_modules=[Extension("hades_extensions", ["hades_extensions"])],
-        script_args=["bdist_wheel"],
-        cmdclass={"build_ext": CMakeBuild},
+    result_path = Path(__file__).parent.joinpath(
+        setup(
+            name="hades_extensions",
+            ext_modules=[Extension("hades_extensions", ["hades_extensions"])],
+            script_args=["bdist_wheel"],
+            cmdclass={"build_ext": CMakeBuild},
+        ).dist_files[0][2],
     )
-    subprocess.run(
-        f'pip install --force-reinstall "{Path.cwd().joinpath(dist.dist_files[0][2])}"',
-        check=True,
-    )
+    subprocess.run(f'pip install --force-reinstall "{result_path}"', check=True)
 
 
 if __name__ == "__main__":
