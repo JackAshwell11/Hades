e1db5f2edc13857b6e00008613b980f322aa8901
Jack Ashwell <jack.ashwell1@gmail.com>
2024-06-30 12:19:54 +0100

    Switched to using an environment variable `ARCADE_HEADLESS` for headless Arcade and enabled the environment variable `LIBGL_ALWAYS_SOFTWARE` on Ubuntu to allow the UI tests to run. This also requires disabling the Pyglet shadow window during testing.

--- src/hades/__init__.py
+++ src/hades/__init__.py
@@ -3,22 +3,14 @@
 from __future__ import annotations
 
 # Builtin
-import os
 from pathlib import Path
 
 # Pip
 import arcade
-import pyglet
 
 __author__ = "Aspect1103"
 __license__ = "GNU GPLv3"
 __version__ = "0.1.0"
-
-# Test if we're running the tests on Linux in CI
-if (
-    os.getenv("GITHUB_ACTIONS") == "true" and os.getenv("RUNNER_OS") == "Linux"
-):  # pragma: no cover
-    pyglet.options["headless"] = True
 
 
 # Add the resources directory to the resource loader
