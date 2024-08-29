3e9c6069a7b6b23921f40dd14291009aa7ca498c
Jack Ashwell <jack.ashwell1@gmail.com>
2024-08-29 14:17:43 +0100

    Switched to using `scikit-build-core` as the build-backend allowing for `build_cpp_extensions()` to be removed simplifying the extensions process.

--- noxfile.py
+++ noxfile.py
@@ -4,11 +4,8 @@ from __future__ import annotations
 
 # Builtin
 import platform
-import shutil
 import subprocess
-import sys
 import zipfile
-from contextlib import suppress
 from pathlib import Path
 
 # Pip
@@ -16,57 +13,6 @@ from nox import Session, options, session
 
 # Define which sessions should be run
 options.sessions = ["tests"]
-
-
-def build_cpp_extensions(
-    python_path: str = sys.prefix,
-    python_version: str = f"{sys.version_info.major}.{sys.version_info.minor}",
-) -> None:
-    """Compiles the C++ extensions and installs them into the virtual environment.
-
-    Args:
-        python_path: The Python environment to install the C++ extensions into.
-        python_version: The version of Python to install the C++ extensions into.
-    """
-    # Make sure the build directory is empty
-    build_dir = Path(__file__).parent / "dist"
-    if build_dir.exists():
-        shutil.rmtree(build_dir)
-
-    # Compile and build the CMake extension
-    os_type = platform.system()
-    subprocess.run(
-        [
-            "cmake",
-            str(Path(__file__).parent / "src" / "hades_extensions"),
-            f"-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_RELEASE={build_dir}",
-            "-DDO_TESTS=OFF",
-            "--fresh",
-            "--preset",
-            "WinRelease" if os_type == "Windows" else "LinuxRelease",
-        ],
-        check=True,
-    )
-    subprocess.run(
-        ["cmake", "--build", "src/hades_extensions/build-release"],
-        check=True,
-    )
-
-    # Move the compiled files to the site-packages directory
-    site_packages_dir = Path(python_path) / (
-        "Lib/site-packages"
-        if os_type == "Windows"
-        else f"lib/python{python_version}/site-packages"
-    )
-    for file in build_dir.glob("*"):
-        with suppress(shutil.Error):
-            shutil.move(file, site_packages_dir / file.name)
-
-
-@session()  # type: ignore[misc]
-def cpp(_: Session) -> None:
-    """Compiles the C++ extensions and installs them into the virtual environment."""
-    build_cpp_extensions()
 
 
 @session()  # type: ignore[misc]
@@ -92,19 +38,16 @@ def executable(executable_session: Session) -> None:
     dist_dir = output_dir / "window.dist"
 
     # Execute the build command
-    build_cpp_extensions(
-        executable_session.virtualenv.location,
-        executable_session.python,
-    )
     subprocess.run(
         [
-            f"{executable_session.virtualenv.bin}/python -m",
-            f'nuitka "{source_dir}"',
+            f"{executable_session.virtualenv.bin}/python",
+            "-m",
+            "nuitka",
+            str(source_dir),
             "--standalone",
             "--assume-yes-for-downloads",
             "--include-data-dir=src/hades/resources=hades/resources",
-            "--include-module=hades_extensions",
-            f'--output-dir="{output_dir}"',
+            f"--output-dir={output_dir}",
             "--remove-output",
         ],
         check=True,
@@ -124,6 +67,5 @@ def tests(test_session: Session) -> None:
         test_session: The Nox session to run the tests with.
     """
     test_session.install(".", "pytest-cov")
-    build_cpp_extensions(test_session.virtualenv.location, test_session.python)
     test_session.run("pytest", "--cov-append")
     test_session.run("coverage", "lcov")
