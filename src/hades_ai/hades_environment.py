f4d3f66fd2856e2446c3bcfbf976e64c34d6ea2c
Jack Ashwell <jack.ashwell1@gmail.com>
2024-10-06 20:41:45 +0100

    Introduced the dungeon generation with a random seed each reset. The AI can now train generically to avoid the walls in randomly generated level improving its capabilities.
    Moved `wall_distances()` to the `PhysicsEngine` removing the need for getting the space.
    
    Enabled saving of the graphs every `SAVE_INTERVAL`. This combines with the functionality of the video recording to create a new directory for each interval.
    
    Tuned hyperparameters to reduce memory footprint while maintaining performance.
    
    Introduced a new `epsilon` attribute to `DQNAgent` to remove the need for `eps_threshold`.

--- src/hades_ai/hades_environment.py
+++ src/hades_ai/hades_environment.py
@@ -7,28 +7,33 @@ from typing import TYPE_CHECKING, Any, Final, SupportsFloat
 
 # Pip
 import numpy as np
-from arcade import key
+from arcade import get_sprites_at_point, key
 from gymnasium import Env
 from gymnasium.spaces import Box, Dict, Discrete
-from pyglet import app, clock
+from pyglet import clock
 
 # Custom
 from hades.views.game import Game
 from hades_ai.capture_window import CaptureWindow
-from hades_extensions.ecs import SPRITE_SIZE, GameObjectType, Vec2d, wall_distances
+from hades_extensions.ecs import (
+    SPRITE_SIZE,
+    GameObjectType,
+    Vec2d,
+)
 from hades_extensions.ecs.components import KinematicComponent
+from hades_extensions.ecs.systems import PhysicsSystem
 
 if TYPE_CHECKING:
     from gymnasium.core import ActType, ObsType
-    from numpy.typing import NDArray
-
-    from hades_extensions.ecs import Space
 
 # The rate at which to draw the game
 DRAW_RATE: Final[float] = 1 / 60
 
-# The scaling factor for incentivising movement towards the goal
-GOAL_SCALING_FACTOR: Final[float] = 0.025
+# The safe distance to be from a wall
+SAFE_DISTANCE: Final[float] = SPRITE_SIZE * 1.5
+
+# The size of the position history
+POSITION_HISTORY_SIZE: Final[int] = 50
 
 
 class HadesEnvironment(Env):
@@ -37,6 +42,10 @@ class HadesEnvironment(Env):
     Attributes:
         action_space: The action space of the environment.
         observation_space: The observation space of the environment.
+        window: The window to capture the game view.
+        game: The game view.
+        previous_action: The previous action taken.
+        position_history: The history of the player's positions.
 
     Action Space:
         The action space is a discrete space with the following actions:
@@ -56,36 +65,33 @@ class HadesEnvironment(Env):
         - current_velocity: The current velocity of the player.
         - distance_to_walls: The distances from the player to the walls in each
           direction.
-        - distance_to_goal: The Euclidean distance to the goal.
-        - direction_to_goal: The direction vector from the player to the goal.
         - previous_action: The previous action taken.
+        - is_near_wall: A flag indicating if the player is near a wall or not.
 
     Info Space:
-        The info space is a dictionary with the following information:
-        - wall_positions: The positions of the walls in the environment.
-        - goal_position: The position of the goal in the environment.
+        The info space is an empty dictionary.
 
     Reward Function:
         The reward function is as follows:
-        - The agent is incentivized to move towards the goal based on the dot
-          product of their current velocity and the direction to the goal.
-        - The agent is penalised for touching a wall.
-        - The agent is rewarded for reaching the goal.
+        - Start at a neutral reward of 0.5.
+        - The agent is heavily penalised for touching a wall
+        - The agent is penalised for being close to a wall.
+        - The agent is penalised for being stationary for too long.
+        - The agent is penalised for oscillating on the same tile.
+        - The agent is rewarded for moving around.
+        - The agent is rewarded for being away from walls.
 
     Done Function:
         The done function is as follows:
-        - The agent is done if they touch a wall.
-        - The agent is done if they reach the goal.
+        - The agent is done if they are outside the dungeon or inside a wall.
     """
 
     __slots__ = (
         "_action_to_direction",
         "action_space",
         "game",
-        "kinematic_component",
         "observation_space",
         "previous_action",
-        "space",
         "target_position",
         "window",
     )
@@ -124,31 +130,22 @@ class HadesEnvironment(Env):
                     shape=(2,),
                     dtype=np.float32,
                 ),
-                "direction_to_goal": Box(low=-1, high=1, shape=(2,), dtype=np.float32),
-                "distance_to_goal": Box(
-                    low=0,
-                    high=np.inf,
-                    shape=(1,),
-                    dtype=np.float32,
-                ),
                 "distance_to_walls": Box(
                     low=0,
                     high=np.inf,
                     shape=(8,),
                     dtype=np.float32,
                 ),
-                "previous_action": Discrete(9),  # TODO: Is this needed?
+                "previous_action": Discrete(9),
+                "is_near_wall": Discrete(2),
             },
         )
-        # TODO: Maybe add steering force
 
         # Store some variables for the environment
         self.window: CaptureWindow = CaptureWindow()
         self.game: Game | None = None
-        self.kinematic_component: KinematicComponent | None = None
         self.previous_action: int = 0
-        self.space: Space | None = None
-        self.target_position: NDArray[float] = np.array([0.0, 0.0], dtype=np.float32)
+        self.position_history: list[tuple[int, int]] = []
 
     def _get_obs(self: HadesEnvironment) -> ObsType:
         """Returns the current observation.
@@ -159,77 +156,42 @@ class HadesEnvironment(Env):
         Returns:
             The current observation.
         """
-        # Check if the kinematic component is initialised or not
-        if not self.kinematic_component:
-            error = "Kinematic component is not initialised"
+        # Check if the game is initialised or not
+        if not self.game:
+            error = "Game is not initialised."
             raise ValueError(error)
 
-        # Check if the space is initialised or not
-        if not self.space:
-            error = "Chipmunk2D space is not initialised"
-            raise ValueError(error)
-
-        # Get the current position as a numpy array
+        # Calculate the distances to the walls
+        kinematic_component = self.game.registry.get_component(
+            self.game.player,
+            KinematicComponent,
+        )
         current_position = np.array(
-            self.kinematic_component.get_position(),
+            kinematic_component.get_position(),
             dtype=np.float32,
         )
-
-        # Get the Euclidean distance to the goal
-        distance_to_goal = np.linalg.norm(self.target_position - current_position)
+        wall_distances = np.array(
+            [
+                np.linalg.norm(
+                    np.array([wall.x, wall.y], dtype=np.float32) - current_position,
+                )
+                for wall in self.game.registry.get_system(
+                    PhysicsSystem,
+                ).get_wall_distances(Vec2d(*kinematic_component.get_position()))
+            ],
+            dtype=np.float32,
+        )
 
         # Return the observations
         return {
             "current_position": current_position,
             "current_velocity": np.array(
-                self.kinematic_component.get_velocity(),
+                kinematic_component.get_velocity(),
                 dtype=np.float32,
             ),
-            "direction_to_goal": (
-                (self.target_position - current_position) / distance_to_goal
-            ),
-            "distance_to_goal": np.array(
-                [distance_to_goal],
-                dtype=np.float32,
-            ),
-            "distance_to_walls": np.array(
-                [
-                    np.linalg.norm(
-                        np.array([wall.x, wall.y], dtype=np.float32) - current_position,
-                    )
-                    for wall in wall_distances(
-                        self.space,
-                        Vec2d(*self.kinematic_component.get_position()),
-                    )
-                ],
-                dtype=np.float32,
-            ),
+            "distance_to_walls": wall_distances,
             "previous_action": self.previous_action,
-        }
-
-    def _get_info(self: HadesEnvironment) -> dict[str, Any]:
-        """Returns the current information.
-
-        Raises:
-            ValueError: If the game is not initialised.
-
-        Returns:
-            The current information.
-        """
-        # Check if the game is initialised or not
-        if not self.game:
-            error = "Game is not initialised"
-            raise ValueError(error)
-
-        # Return information that could be useful for the agent but is not part of the
-        # observation
-        return {
-            "wall_positions": [
-                wall.position
-                for wall in self.game.tile_sprites
-                if wall.constructor.game_object_type == GameObjectType.Wall
-            ],
-            "goal_position": np.array(self.target_position, dtype=np.float32),
+            "is_near_wall": float(np.any(wall_distances <= (SPRITE_SIZE / 2))),
         }
 
     def reset(
@@ -253,19 +215,14 @@ class HadesEnvironment(Env):
         # Reset the window and store the required variables
         self.previous_action = 0
         self.game = Game(0)
-        self.kinematic_component = self.game.registry.get_component(
-            self.game.player,
-            KinematicComponent,
-        )
-        self.space = self.game.registry.get_space()
-        self.target_position = np.array(self.game.item_sprites[0].position)
+        self.position_history.clear()
 
         # Show the game view and render it so that the AI agent can interact with it
         self.window.show_view(self.game)
         self.render()
 
         # Return the initial observation and information
-        return self._get_obs(), self._get_info()
+        return self._get_obs(), {}
 
     def step(
         self,
@@ -280,7 +237,7 @@ class HadesEnvironment(Env):
             ValueError: If the game is not initialised.
 
         Returns:
-            The observation, reward, done flag, info, and truncated flag.
+            The observation, reward, done flag, truncated flag, and info.
         """
         # Check if the game is initialised or not
         if not self.game:
@@ -298,46 +255,72 @@ class HadesEnvironment(Env):
         # Update the game and render it
         self.render()
 
-        # Get observations and then save the action
+        # Get the observations and store the previous action
         observations = self._get_obs()
         self.previous_action = action
 
-        # Incentivize moving towards the goal based on their current velocity
-        reward = (
-            np.dot(observations["direction_to_goal"], observations["current_velocity"])
-            * GOAL_SCALING_FACTOR
+        # Check if the agent is outside the dungeon
+        sprites = get_sprites_at_point(
+            self.game.registry.get_component(
+                self.game.player,
+                KinematicComponent,
+            ).get_position(),
+            self.game.tile_sprites,
         )
+        if not sprites or sprites[0].game_object_type == GameObjectType.Wall:
+            return observations, 0, False, True, {}
 
-        # If we're touching a wall, we're done
-        done = False
-        if min(observations["distance_to_walls"]) <= SPRITE_SIZE / 2:
-            reward -= 5
-            done = True
+        # Add the current position to the position history
+        current_position = (
+            observations["current_position"][0] // SPRITE_SIZE,
+            observations["current_position"][1] // SPRITE_SIZE,
+        )
+        self.position_history.append(current_position)
 
-        # If we're at the goal, we're also done
-        if observations["distance_to_goal"] <= SPRITE_SIZE / 2:
-            reward += 5
-            done = True
+        # Start with a neutral reward to encourage exploration
+        reward = 0.5
 
-        # Return the results of this step
-        return observations, reward, done, False, self._get_info()
+        # Determine if the agent is near a wall or not
+        # TODO: These rewards need a bit of tweaking
+        min_distance_to_wall = np.min(observations["distance_to_walls"])
+        if min_distance_to_wall <= SPRITE_SIZE / 2:
+            # Strong penalty for touching a wall
+            reward = 0.0
+        elif min_distance_to_wall < SAFE_DISTANCE:
+            # Penalty for being too close to the wall based on proximity to the wall
+            reward -= 0.3 * (1 - (min_distance_to_wall / SAFE_DISTANCE))
+        else:
+            # Reward for being safely away from walls
+            reward += 0.2
 
-    def render(self: HadesEnvironment) -> None:  # noqa: PLR6301
+        # Determine if the agent is moving around or not
+        if len(self.position_history) > POSITION_HISTORY_SIZE:
+            # Maintain a fixed size for the history
+            self.position_history.pop(0)
+            if any(pos == current_position for pos in self.position_history):
+                # Penalty for being stationary for too long
+                reward -= 0.2
+            elif self.position_history.count(current_position) > 5:
+                # Penalty for oscillating on the same tile
+                reward -= 0.1
+            else:
+                # Reward for moving around
+                reward += 0.3
+
+        # Return the observations, reward, done flag, truncated flag, and info
+        return observations, reward, False, False, {}
+
+    def render(self: HadesEnvironment) -> None:
         """Renders the environment."""
         # Update the pyglet clock
         clock.tick()
 
-        # Call the events for each window and update them
-        for window in app.windows:
-            window.switch_to()
-            window.dispatch_events()
-            window.dispatch_event("on_draw")
-            window.flip()
+        # Call the events for the window and render it
+        self.window.switch_to()
+        self.window.dispatch_events()
+        self.window.dispatch_event("on_draw")
+        self.window.flip()
 
     def close(self: HadesEnvironment) -> None:
         """Closes the environment."""
         self.window.close()
-
-
-# TODO: Look at removing goal
-# TODO: Introduce dungeon generation
