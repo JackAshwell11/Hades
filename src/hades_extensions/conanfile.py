390288d44abba2b7d99c9a0d4f4bf47e5e035791
Jack Ashwell <jack.ashwell1@gmail.com>
2025-04-27 18:48:57 +0100

    Switched to Conan 2 C++ dependency management, eliminating the need for Git submodules and platform-specific presets.
    This also paves the way for external CMake files (like sanitisers) now that we have a dedicated cmake directory.

---
+++ src/hades_extensions/conanfile.py
@@ -0,0 +1,26 @@
+"""Manages the Conan configuration for the application."""
+
+from __future__ import annotations
+
+# Builtin
+from typing import TYPE_CHECKING
+
+# Pip
+from conan import ConanFile
+
+if TYPE_CHECKING:
+    from typing import ClassVar
+
+
+class HadesExtensions(ConanFile):  # type: ignore[misc]
+    """The Conan configuration for the application."""
+
+    # The metadata for the package
+    generators: ClassVar[list[str]] = ["CMakeDeps"]
+    settings: ClassVar[list[str]] = ["build_type"]
+
+    def requirements(self: ConanFile) -> None:
+        """Add the required dependencies to the Conan configuration."""
+        self.requires("pybind11/2.13.6")
+        self.requires("gtest/1.16.0")
+        self.requires("chipmunk2d/7.0.3")
